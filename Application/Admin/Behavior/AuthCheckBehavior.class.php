785f2f50de15d448efedecb93aea1e4392f9ed32
631248045@qq.com <631248045@qq.com>
2015-12-25 17:26:10 +0800

    initing

---
+++ Application/Admin/Behavior/AuthCheckBehavior.class.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Admin\Behavior;
+use Think\Behavior;
+
+defined('THINK_PATH') or exit();
+
+class AuthCheckBehavior extends Behavior {
+    protected $config;
+    public function run(&$params) {
+        
+    }
+}
+?>
