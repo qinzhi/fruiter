f78b586aa97bfc3a9dbe633e8e7ffe56241f20b1
631248045@qq.com <631248045@qq.com>
2015-12-28 18:57:01 +0800

    .

--- Application/Admin/Controller/PublicController.class.php
+++ Application/Admin/Controller/PublicController.class.php
@@ -6,7 +6,7 @@
  * Time: 下午4:20
  */
 namespace Admin\Controller;
-use Think\Controller;
+use Think\Controller,Think\Verify;
 class PublicController extends Controller{
 
     public $config = array();
@@ -23,17 +23,16 @@ class PublicController extends Controller{
 
     public function captcha(){
         $config = array(
-            'width' => '100',   //验证码宽度 设置为0为自动计算
-            'height' => '32',   //验证码高度 设置为0为自动计算
-            'fontSize' => 30,    // 验证码字体大小
-            'maxWordLength' =>    4,     // 验证码位数
-            'minWordLength' => 4, //验证码背景颜色 rgb数组设置，例如 array(243, 251, 254)
+            'imageW' => '100',   //验证码宽度 设置为0为自动计算
+            'imageH' => '32',   //验证码高度 设置为0为自动计算
+            'fontSize' => 14,    // 验证码字体大小
+            'useNoise'    =>    false, // 是否添加杂点 默认为true
+            'useCurve' => false,    //是否使用混淆曲线 默认为true
+            'length' =>    4,     // 验证码位数
+            'useImgBg' => false,    //是否使用背景图片 默认为false
+            'bg' => array(255, 255, 255), //验证码背景颜色 rgb数组设置，例如 array(243, 251, 254)
         );
-        $captcha = new \Common\Library\Org\Util\Captcha($config);
-        //$captcha->CreateImage($text);
-        //fb($text);
-        //session('captcha',$text);
-		$verify = new \Think\Verify();
+		$verify = new Verify($config);
         $verify->entry();
     }
 }
